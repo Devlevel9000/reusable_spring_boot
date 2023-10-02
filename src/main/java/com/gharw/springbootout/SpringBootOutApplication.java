468d388714e46ab2d51c7912cd1280bea69d8954
DevLevel9000 <zeusakaeden@gmail.com>
2023-10-02 16:42:31 +0300

    Initial commit...

---
+++ src/main/java/com/gharw/springbootout/SpringBootOutApplication.java
@@ -0,0 +1,13 @@
+package com.gharw.springbootout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gharw/springbootout/SpringBootOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gharw.springbootout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
